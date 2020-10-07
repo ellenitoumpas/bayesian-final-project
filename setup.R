8fe89baca39cc7b3c02baee1f2d94fd9b3f55bac
Phil Steinke <phil@tgrrr.com>
2020-10-07 16:05:55 +0800

    fix: update setup file to ensure it works across computers

--- setup.R
+++ setup.R
@@ -1,12 +1,20 @@
-if (!requireNamespace("remotes"))
-  install.packages("remotes")
+# if (!requireNamespace('remotes'))
+#   install.packages('remotes')
 
-remotes::install_github("rstudio/renv")
+# remotes::install_github('rstudio/renv')
 
-if (!requireNamespace("here"))
-  install.packages("here")
+if (!requireNamespace('renv'))
+  install.packages('renv')
+library('renv')
 
-packages <- scan("requirements.txt", what="", sep="\n")
+if (!requireNamespace('here'))
+  install.packages('here')
+library('here')
+
+# renv::init()
+
+packages <- scan('requirements.txt', what="", sep='\n')
+source(here('functions', 'functions_packages.R'))
 lock_n_load_libraries(packages)
 
 # TODO: "lintr"
