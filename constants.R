97d4a40b949be9aedc96d7265e8fe8b863b5f341
Phil Steinke <phil@tgrrr.com>
2020-10-08 19:41:07 +0800

    move seed_value to constants

--- constants.R
+++ constants.R
@@ -19,3 +19,5 @@ parameters = c( "zbeta0" ,  "zbeta" , "beta0" ,  "beta" ,  "tau", "zVar") # Here
 
 days_of_weeks <- c('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
 wind_directions <- c('N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW','NNW')
+
+seed_value <- sample(1:10000, 20, replace=F)
