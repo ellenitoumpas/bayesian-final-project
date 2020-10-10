3889f9bf847e663e800cd9ccddcff80b8a741210
Elleni Toumpas <ellenitoumpas@MacBook-Pro.lan>
2020-10-10 11:49:17 +1100

    Model functions half finished.

--- functions/functions_bayesian.R
+++ functions/functions_bayesian.R
@@ -1,20 +1,12 @@
 #' @title Prepare model
 #' @description Creates the model ready to deliver to JAGS
-#' @param beta1_mu prior specification
-#' @param beta1_var prior specification 
-#' @param beta2_mu prior specification
-#' @param beta2_var prior specification
-#' @param beta3_mu prior specification
-#' @param beta3_var prior specification
-#' @param beta4_mu prior specification
-#' @param beta4_var prior specification
-#' @param beta5_mu prior specification
-#' @param beta5_var prior specification
+#' @param mu_list
+#' @param var_list prior specification
+#' @param num_predictions
 #' @return
 #' @export
 #' @examples
-prepare_JAGS_model <- function(beta1_mu, beta1_var, beta2_mu, beta2_var, beta3_mu, beta3_var, beta4_mu, beta4_var, beta5_mu, beta5_var, 
-                               beta6_mu, beta6_var, beta7_mu, beta7_var, beta8_mu, beta8_var, num_predictions){
+prepare_JAGS_model <- function(mu_list, var_list, num_predictions){
   
   modelString <- paste0("
   
@@ -26,53 +18,52 @@ prepare_JAGS_model <- function(beta1_mu, beta1_var, beta2_mu, beta2_var, beta3_m
       zy[i] <- y[i] / ysd
     }
     
-  for ( j in 1:Nx ) {
-    xsd[j] <-   sd(x[,j])
-    for ( i in 1:Ntotal ) {
-      zx[i,j] <- x[i,j] / xsd[j]
+    for ( j in 1:Nx ) {
+      xsd[j] <-   sd(x[,j])
+      for ( i in 1:Ntotal ) {
+        zx[i,j] <- x[i,j] / xsd[j]
+      }
     }
   }
-}
 
   # Outlining the model
-  
   model{
-  
     for (i in 1:Ntotal) {
       zy[i] ~ dgamma((mu[i]^2)/ zVar, mu[i]/ zVar) # Use sample variance as an estimate of the population variance
       mu[i] <- zbeta0 + sum(zbeta[1:Nx] * zx[i,1:Nx])
     }
     
-    zbeta0 ~ dnorm(0, 1/2^2)
-    zbeta[1] ~ dnorm(",beta1_mu,"/xsd[1], 1/(",beta1_var,"/xsd[1]^2))
-    zbeta[2] ~ dnorm(",beta2_mu,"/xsd[2], 1/(",beta2_var,"/xsd[2]^2))
-    zbeta[3] ~ dnorm(",beta3_mu,"/xsd[3], 1/(",beta3_var,"/xsd[3]^2))
-    zbeta[4] ~ dnorm(",beta4_mu,"/xsd[4], 1/(",beta4_var,"/xsd[4]^2))
-    zbeta[5] ~ dnorm(",beta5_mu,"/xsd[5], 1/(",beta5_var,"/xsd[5]^2))
-    zbeta[6] ~ dnorm(",beta6_mu,"/xsd[6], 1/(",beta5_var,"/xsd[6]^2))
-    zbeta[7] ~ dnorm(",beta7_mu,"/xsd[7], 1/(",beta5_var,"/xsd[7]^2))
-    zbeta[8] ~ dnorm(",beta8_mu,"/xsd[8], 1/(",beta5_var,"/xsd[8]^2))
-    zVar ~ dgamma(0.01, 0.001)
-    
-    # Transform to original scale
-    beta[1:Nx] <- (zbeta[1:Nx]/ xsd[1:Nx]) * ysd
-    beta0 <- zbeta0 * ysd
-    tau <- zVar * (ysd) ^ 2
-    
-    # Predictions
-    pred[1] <- beta0 + beta[1] * xPred[1, 1] + beta[2] * xPred[1,2] + beta[3] * xPred[1,3] + beta[4] * xPred[1,4] + beta[5] * xPred[1,5] + beta[6] * xPred[1,6] + beta[7] * xPred[1, 7] + beta[8] * xPred[1, 8]    
-    pred[2] <- beta0 + beta[1] * xPred[2, 1] + beta[2] * xPred[2,2] + beta[3] * xPred[2,3] + beta[4] * xPred[2,4] + beta[5] * xPred[2,5] + beta[6] * xPred[2,6] + beta[7] * xPred[2, 7] + beta[8] * xPred[2, 8]    
-    pred[10] <- beta0 + beta[1] * xPred[10, 1] + beta[2] * xPred[10,2] + beta[3] * xPred[10,3] + beta[4] * xPred[10,4] + beta[5] * xPred[10,5] + beta[6] * xPred[10,6] + beta[7] * xPred[10, 7] + beta[8] * xPred[10, 8]    
-    pred[20] <- beta0 + beta[1] * xPred[20, 1] + beta[2] * xPred[20,2] + beta[3] * xPred[20,3] + beta[4] * xPred[20,4] + beta[5] * xPred[20,5] + beta[6] * xPred[20,6] + beta[7] * xPred[20, 7] + beta[8] * xPred[20, 8]    
-    pred[30] <- beta0 + beta[1] * xPred[30, 1] + beta[2] * xPred[30,2] + beta[3] * xPred[30,3] + beta[4] * xPred[30,4] + beta[5] * xPred[30,5] + beta[6] * xPred[30,6] + beta[7] * xPred[30, 7] + beta[8] * xPred[30, 8]    
-    pred[40] <- beta0 + beta[1] * xPred[40, 1] + beta[2] * xPred[40,2] + beta[3] * xPred[40,3] + beta[4] * xPred[40,4] + beta[5] * xPred[40,5] + beta[6] * xPred[40,6] + beta[7] * xPred[40, 7] + beta[8] * xPred[40, 8]    
-    pred[50] <- beta0 + beta[1] * xPred[50, 1] + beta[2] * xPred[50,2] + beta[3] * xPred[50,3] + beta[4] * xPred[50,4] + beta[5] * xPred[50,5] + beta[6] * xPred[50,6] + beta[7] * xPred[50, 7] + beta[8] * xPred[50, 8]    
-    pred[60] <- beta0 + beta[1] * xPred[60, 1] + beta[2] * xPred[60,2] + beta[3] * xPred[60,3] + beta[4] * xPred[60,4] + beta[5] * xPred[60,5] + beta[6] * xPred[60,6] + beta[7] * xPred[60, 7] + beta[8] * xPred[60, 8]    
-    pred[70] <- beta0 + beta[1] * xPred[70, 1] + beta[2] * xPred[70,2] + beta[3] * xPred[70,3] + beta[4] * xPred[70,4] + beta[5] * xPred[70,5] + beta[6] * xPred[70,6] + beta[7] * xPred[70, 7] + beta[8] * xPred[70, 8]    
-    
-  }" )
+  zbeta0 ~ dnorm(0, 1/2^2)")
   
-  writeLines(modelString, con = "TEMPmodel.txt") # write to file
+  for(i in 1:length(mu_list)){
+      modelString <- paste0(modelString, "\n  zbeta[",i,"] ~ dnorm(",mu_list[i],"/xsd[",i,"], 1/(",var_list[i],"/xsd[",i,"]^2))")
+  }
+  
+             
+  modelString <- paste0(modelString,
+  "\n  zVar ~ dgamma(0.01, 0.001)
+  
+  # Transform to original scale
+  beta[1:Nx] <- (zbeta[1:Nx]/ xsd[1:Nx]) * ysd
+  beta0 <- zbeta0 * ysd
+  tau <- zVar * (ysd) ^ 2
+  
+  # Predictions
+  ")
+
+  # Write predictions
+  for(pred in 1:num_predictions){
+    modelString <- paste0(modelString, "\n  pred[",pred,"] <- beta0 ")
+    for(mu in 1:length(mu_list)){
+      modelString <- paste0(modelString, "+ beta[",mu,"] * xPred[",pred,", ",mu,"] ")
+    }
+  }
+  
+  # Finish model string
+  modelString <- paste0(modelString,"\n\n}")
+  
+  # Write to file
+  writeLines(modelString, con = "TEMPmodel.txt") 
   
 }
 
